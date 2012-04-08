4076d530e6cb93dc745a8cf5a00e303b7aa19dd0
doug y'barbo <doug.ybarbo@gmail.com>
2012-04-08 01:22:17 -0700

    added config.py with some initial content

---
+++ data-scope/config.py
@@ -0,0 +1,9 @@
+DEBUG=True
+REDIS_DB = 2
+DEBUG = True
+SECRET_KEY = "!Erew9reQir549&3d394W*"
+USERNAME = None
+PASSWORD = None
+REDIS_HOST = 'localhost'
+PORT = 6379
+RESOURCE_PATH="/Users/doug/Dropbox/WebDev"
